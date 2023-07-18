b32bfdeccd7acc777b3d2e82497645fc2e34a6b9
Dhiaa Naas <dnaas056@uottawa.ca>
2023-07-18 13:23:52 -0400

    Added visual support for week view Next step is to fill the boxes that represent a busy timeslots in the calendar

--- src/Schedules.js
+++ src/Schedules.js
@@ -1,12 +1,48 @@
+import "./Schedules.css";
+
 function Schedules() {
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const hours = Array.from({ length: 13 }, (_, i) => i);
+
   return (
-    <div>
-      <p>
-        Schedules display - this is to be replaced with the Schedules view
-        implementation
-      </p>
+    <div className="bigDiv">
+      <div className="row">
+        <div className="hours"></div>
+        <div className="weekdays">
+          {days.map((day) => (
+            <div key={day} className="day">
+              {day}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="row">
+        <div className="hours">
+          {hours.map((hour) => (
+            <div key={hour} className="hour">
+              {hour+8}:00
+            </div>
+          ))}
+        </div>
+        <div className="week-schedule">
+          <div className="schedule">
+            {days.map((day) => (
+              <div key={day} className="day-schedule">
+                {hours.map((hour) => (
+                  <div key={`${day}-${hour}`} className="hour-slot">
+                    {/* Display your schedule data here */ "wasgud"}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
     </div>
+
+
   );
-}
+};
 
 export default Schedules;
