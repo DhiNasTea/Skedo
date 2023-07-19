a101f134f3b324814d435899fa5f6d86dc55d486
Miten Soni <msoni099@uottawa.ca>
2023-07-18 21:52:32 -0400

    new tab

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Options from "./Options";
 import Schedules from "./Schedules";
+import SavedSchedules from "./SavedSchedules";
 import Tasks from "./Tasks";
 
 const Tabs = () => {
@@ -48,11 +49,24 @@ const Tabs = () => {
         >
           Schedules
         </a>
+        <a
+          className={
+            activeTab === "saved_schedules"
+              ? "active nav-item nav-link"
+              : "nav-item nav-link"
+          }
+          onClick={() => handleTabClick("saved_schedules")}
+          data-toggle="tab"
+          href="#saved_schedules"
+        >
+          View Saved Schedules
+        </a>
       </nav>
       <div className="tab-content">
         {activeTab === "tasks" && <Tasks />}
         {activeTab === "options" && <Options />}
         {activeTab === "schedules" && <Schedules />}
+        {activeTab === "saved_schedules" && <SavedSchedules />}
       </div>
     </div>
   );
